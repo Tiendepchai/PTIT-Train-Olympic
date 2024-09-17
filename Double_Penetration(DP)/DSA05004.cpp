cdd7cf92e15317eaed96110f22506997309129bb
Tiendepchai <chodienkk@icloud.com>
2024-09-17 13:48:46 +0700

    nakba

--- Double_Penetration(DP)/DSA05004.cpp
+++ Double_Penetration(DP)/DSA05004.cpp
@@ -20,8 +20,12 @@ signed main(){
   for (int i = 1; i <=n; i++) cin >> a[i];
   int ans = 1;
   for (int i = 1; i <= n; i++){
-    for (int j = 1; j < i; j++){
-      if (a[i] > a[j]) cout << j << ' ' << i << '\n';
+    for (int j = i - 1; j >= 1; j--){
+      if (a[i] > a[j]) {
+        dp[i] = max(dp[i], dp[j] + 1);
+      }
+      ans = max(ans, dp[i]);
     }
   }
+  cout << ans;
 }
