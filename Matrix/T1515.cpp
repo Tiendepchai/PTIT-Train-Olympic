c191fa9c051440d3b457bda4766ceacedabd85a7
Tiendepchai <chodienkk@icloud.com>
2024-11-05 15:55:36 +0700

    Yes sir

--- Matrix/T1515.cpp
+++ Matrix/T1515.cpp
@@ -20,6 +20,7 @@ class matrix{
     matrix(int row, int col) : row(row), col(col) {
       arr.resize(row, vector<int>(col, 0));
     }
+    
     matrix operator * (matrix const& obj){
       matrix res(row, obj.col);
       for (int x = 0; x < row; x++){
@@ -48,18 +49,31 @@ matrix fast_power(matrix const &aa, int n){
   return (n&1?tmp*tmp*aa:tmp*tmp);
 }
 
+matrix B(matrix &a, int k){
+  if (k == 1) return a;
+  int n = a.row;
+  matrix I(n, n);
+  
+  for (int i = 0; i < n; i++){
+    for (int j = 0; j < n; j++){
+      if (i == j) I.arr[i][j] = 1;
+    }
+  } 
+  matrix tmp = I + fast_power(a, k/2);
+  if (k%2==0) return tmp*B(a, k/2);
+  return tmp*B(a, k/2) + fast_power(a, k);
+}
+
 signed main()
 {
   int n, power; cin >> n >> power;
-  matrix a(n, n), ans(n, n), base(n, n);
+  matrix a(n, n);
   for (int i = 0; i < n; i++){
     for (int j = 0; j < n; j++){
       cin >> a.arr[i][j];
     }
-  } 
-  for (int i = 1; i <= power; i++){
-    ans = ans + fast_power(a, i);
   }
+  matrix ans = B(a,power);
   for (int x = 0; x < n; x++){
     for (int y = 0; y < n; y++){
       cout << ans.arr[x][y] << ' ';
